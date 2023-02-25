fe4601ae552782a1a9682cc7499ddb6a3082d1d9
realVinayak <realvinayak123@gmail.com>
2023-02-24 19:59:03 -0600

    Update readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # PHSX815_Week6
+Calculates integral of $x^2sin(x)$ in integral 4 to 7 with 300 interval.
+Set the lower bound of integration as `min_limit`. Set the upper bound of the integration as `max_limit`. Set the number of intervals as `N_inter`. The variables are definition in `driver.py`.
+<br/>
+To run code, type `python3 driver.py`
--- simple.py
+++ simple.py
@@ -1,16 +1,12 @@
-
-
-
-
-
-def rectange_rule(funct_to_evaluate, min_value, max_value, N_meas):
-    step_size = (max_value - min_value)/N_meas
+def rectangle_rule(funct_to_evaluate, min_value, max_value, N_meas):
+    step_size = (max_value - min_value) / N_meas
     integral = 0
     prev_val = min_value
     for index in range(N_meas):
         integral += funct_to_evaluate([prev_val])[0]
         prev_val += step_size
-    return integral*step_size
+    return integral * step_size
+
 
 def simple(min_limit, max_limit, N_inter, func_to_evaluate):
-    return rectange_rule(func_to_evaluate, min_limit, max_limit, N_inter)
+    return rectangle_rule(func_to_evaluate, min_limit, max_limit, N_inter)
